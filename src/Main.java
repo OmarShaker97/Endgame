a312a0aa7f29d70979a8d6468226cd1eced1c31d
OmarShaker97 <34036869+OmarShaker97@users.noreply.github.com>
2019-10-04 15:41:02 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 
-public class Main {
+public class Main{
+	
+	Problem problem;
 	
 	public static void main(String[] args) {
-	
+		// problem = new Endgame();
 	}
 	
 	public static String[][] StringtoArray(String grid) {
@@ -14,16 +17,30 @@ public class Main {
 		String[][] stonesCoordinatesArray = new String[6][2];
 		String[] warriorsCoordinatesString = inputString[4].split(",");
 		String[][] warriorsCoordinatesArray = new String[warriorsCoordinatesString.length/2][2];
+		
 		for(int i = 0; i<stonesCoordinatesString.length; i+=2) {
 			stonesCoordinatesArray[i] = new String[]{stonesCoordinatesString[i],stonesCoordinatesString[i+1]};	
 		}
 		for(int i = 0; i<warriorsCoordinatesString.length; i+=2) {
 			warriorsCoordinatesArray[i] = new String[]{warriorsCoordinatesString[i],warriorsCoordinatesString[i+1]};	
 		}
+		
+		String[][] finalGrid = new String[Integer.parseInt(gridCoordinates[0])][Integer.parseInt(gridCoordinates[1])];
+		
+		for(int i = 0; finalGrid.length>i; i++) {
+			for(int j = 0; finalGrid[i].length>j; j++) {
+				
+			}
+		}
 	}
 	
-	public static String solve(String grid, String strategy, boolean visualize) {
+	public static String solve(String[][] grid, String strategy, boolean visualize) {
 		
+		if(strategy.equals("BF")) {
+			ArrayList<Node> nodes = new ArrayList<Node>();
+			//nodes.add(0, new Node(ironManCoordinates, null, null, 0, 0));
+		}
 		
 	}
+	
 }
